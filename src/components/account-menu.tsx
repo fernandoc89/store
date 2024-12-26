4f44b02ba7b8741ab267724cab645da3117f4624
fernandoc89 <fernandocardoso@live.com>
2024-12-26 17:33:24 -0300

    feature (7): adding profile user and store integration and creating modal to edit store profile

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,32 +1,66 @@
+import { getManagedSore } from "@/api/get-managed-store";
+import { getProfile } from "@/api/get-profile";
+import { useQuery } from "@tanstack/react-query";
 import { Building, ChevronDown } from "lucide-react";
+import { StoreProfileDialog } from "./store-profile-dialog";
 import { Button } from "./ui/button";
+import { Dialog, DialogTrigger } from "./ui/dialog";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
-
+import { Skeleton } from "./ui/skeleton";
 
 export function AccountMenu() {
+    const { data: profile, isLoading: isLoadingProfile } = useQuery({
+        queryKey: ['profile'],
+        queryFn: getProfile,
+        staleTime: Infinity
+    })
+
+    const { data: managedStore, isLoading: isLoadingManagedStore } = useQuery({
+        queryKey: ['managed-store'],
+        queryFn: getManagedSore,
+        staleTime: Infinity
+    })
+
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger >
-                <Button variant="outline" className="flex items-center gap-2 select-none">
-                    Store
-                    <ChevronDown className="w-4 h-4" />
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end" className="w-56">
-                <DropdownMenuLabel className="flex flex-col">
-                    <span>Fernando Cardoso</span>
-                    <span className="text-xs font-normal text-muted-foreground">fernandocardoso@live.com</span>
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                    <Building className="mr-2 w-4 h-4" />
-                    <span>Perfil da loja</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
-                    <Building className="mr-2 w-4 h-4" />
-                    <span>Sair</span>
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+        <Dialog>
+            <DropdownMenu>
+                <DropdownMenuTrigger >
+                    <Button variant="outline" className="flex items-center gap-2 select-none">
+                        {isLoadingManagedStore ? (
+                            <Skeleton className="h-4 w-40" />
+                        ) : managedStore?.name}
+                        <ChevronDown className="w-4 h-4" />
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end" className="w-56">
+                    <DropdownMenuLabel className="flex flex-col">
+                        {isLoadingProfile ? (
+                            <div className="space-y-.5">
+                                <Skeleton className="h-4 w-32" />
+                                <Skeleton className="h-3 w-24" />
+                            </div>
+                        ) : (
+                            <>
+                                <span>{profile?.name}</span>
+                                <span className="text-xs font-normal text-muted-foreground">{profile?.email}</span>
+                            </>
+
+                        )}
+                    </DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DialogTrigger asChild>
+                        <DropdownMenuItem>
+                            <Building className="mr-2 w-4 h-4" />
+                            <span>Perfil da loja</span>
+                        </DropdownMenuItem>
+                    </DialogTrigger>
+                    <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
+                        <Building className="mr-2 w-4 h-4" />
+                        <span>Sair</span>
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+            <StoreProfileDialog />
+        </Dialog>
     )
 }
