1539663b40db14c8414462167fe8d04d3f3ffcc7
FernandoCardosoCD2 <fernando.cardoso@cd2.com.br>
2025-04-28 12:04:51 -0300

    feature (11): adding pagination function

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -5,9 +5,10 @@ export interface PaginationProps {
     pageIndex: number
     totalCount: number
     perPage: number
+    onPageChange: (pageIndex: number) => Promise<void> | void
 }
 
-export function Pagination({ pageIndex, perPage, totalCount }: PaginationProps) {
+export function Pagination({ pageIndex, perPage, totalCount, onPageChange }: PaginationProps) {
     const pages = Math.ceil(totalCount / perPage) || 1;
 
     return (
@@ -21,19 +22,19 @@ export function Pagination({ pageIndex, perPage, totalCount }: PaginationProps) 
                     Página {pageIndex + 1} de {pages}
                 </div>
                 <div className="flex items-center gap-2">
-                    <Button variant="outline" className="h-8 w-8 p-8">
+                    <Button onClick={() => onPageChange(0)} variant="outline" className="h-8 w-8 p-8" disabled={pageIndex === 0}>
                         <ChevronsLeft className="h4- w-4" />
                         <span className="sr-only">Primeira página</span>
                     </Button>
-                    <Button variant="outline" className="h-8 w-8 p-8">
+                    <Button onClick={() => onPageChange(pageIndex - 1)} variant="outline" className="h-8 w-8 p-8" disabled={pageIndex === 0}>
                         <ChevronLeft className="h4- w-4" />
-                        <span className="sr-only">Próxima página</span>
-                    </Button>
-                    <Button variant="outline" className="h-8 w-8 p-8">
-                        <ChevronRight className="h4- w-4" />
                         <span className="sr-only">Página anterior</span>
                     </Button>
-                    <Button variant="outline" className="h-8 w-8 p-8">
+                    <Button onClick={() => onPageChange(pageIndex + 1)} variant="outline" className="h-8 w-8 p-8" disabled={pages <= pageIndex + 1}>
+                        <ChevronRight className="h4- w-4" />
+                        <span className="sr-only">Próxima página</span>
+                    </Button>
+                    <Button onClick={() => onPageChange(pages - 1)} variant="outline" className="h-8 w-8 p-8" disabled={pages <= pageIndex + 1}>
                         <ChevronsRight className="h4- w-4" />
                         <span className="sr-only">Última página</span>
                     </Button>
--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -3,14 +3,26 @@ import { Pagination } from "@/components/pagination";
 import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { useQuery } from "@tanstack/react-query";
 import { Helmet } from "react-helmet-async";
+import { useSearchParams } from "react-router";
+import * as Z from "zod";
 import { OrderTableFilters } from "./order-table-filters";
 import { OrderTableRow } from "./order-table-row";
-
 export function Orders() {
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    const pageIndex = Z.coerce.number().transform(page => page - 1).parse(searchParams.get('page') ?? '1')
+
     const { data: result } = useQuery({
-      queryKey: ['orders'],
-      queryFn: getOrders,
+      queryKey: ['orders', pageIndex],
+      queryFn: () => getOrders({pageIndex}),
     })
+
+    function handlePageChange(pageIndex: number) {
+        setSearchParams(state => {
+            state.set('page', (pageIndex + 1).toString())
+            return state
+        })
+    }
 
     return (
         <>
@@ -42,7 +54,10 @@ export function Orders() {
                             </TableBody>
                         </Table>
                     </div>
-                    <Pagination pageIndex={0} totalCount={105} perPage={10} />
+                    {result && (
+                        <Pagination pageIndex={result.meta.pageIndex} totalCount={result.meta.totalCount} perPage={result.meta.perPage} onPageChange={handlePageChange} />
+                    )}
+
                 </div>
             </div>
         </>
